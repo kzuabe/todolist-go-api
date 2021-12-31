d4b1ec938e0160538f9d25fc5049073414ac7061
kzuabe <ai3ekzk@gmail.com>
2021-12-31 12:18:23 +0900

    docs: コメント修正（タスクのステータスから進行中を削除）

--- internal/entity/task.go
+++ internal/entity/task.go
@@ -5,5 +5,5 @@ type Task struct {
 	UserID      string `json:"user_id"`
 	Title       string `json:"title"`
 	Description string `json:"description"`
-	Status      int    `json:"status"` // 0: 未着手, 1: 進行中, 2: 完了
+	Status      int    `json:"status"` // 0: 未着手, 1: 完了
 }
